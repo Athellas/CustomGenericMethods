e776ae723761395d61b33b9b68073ce20c8780d6
Michal Krupinski <michal.krupinski@fuerogames.pl>
2018-05-11 18:54:22 +0200

    351

--- CustomGenericMethods/Program.cs
+++ CustomGenericMethods/Program.cs
@@ -10,6 +10,45 @@ namespace CustomGenericMethods
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Fun with Custom Generic Methods *****\n");
+
+            int a = 10, b = 90;
+            Console.WriteLine("Before swap: {0}, {1}", a, b);
+            Swap<int>(ref a, ref b);
+            Console.WriteLine("After swap: {0}, {1}", a, b);
+            Console.WriteLine();
+
+            // Swap 2 strings.
+            string s1 = "Hello", s2 = "There";
+            Console.WriteLine("Before swap: {0} {1}!", s1, s2);
+            Swap<string>(ref s1, ref s2);
+            Console.WriteLine("After swap: {0} {1}!", s1, s2);
+            Console.ReadLine();
+        }
+
+        static void Swap(ref int a, ref int b)
+        {
+            int temp;
+            temp = a;
+            a = b;
+            b = temp;
+        }
+
+        static void Swap(ref Person a, ref Person b)
+        {
+            Person temp;
+            temp = a;
+            a = b;
+            b = temp;
+        }
+
+        static void Swap<T>(ref T a, ref T b)
+        {
+            Console.WriteLine($"You sent the Swap() method a {typeof(T)}");
+            T temp;
+            temp = a;
+            a = b;
+            b = temp;
         }
     }
 }
